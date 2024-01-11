29aa23f7949dc81a2fdbfbff26c7ec931dc19b1b
SheikhAman <abamsheikh@gmail.com>
2024-01-11 16:27:16 +0545

    login.php backend fixes

--- register.php
+++ register.php
@@ -2,92 +2,84 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />
-  <meta http-equiv="x-ua-compatible" content="ie=edge" />
-  <title>Health | Template</title>
-  <meta name="description" content="" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="manifest" href="site.webmanifest" />
-  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico" />
-  <!-- Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <title>Register</title>
+  <!-- <link rel="stylesheet" href="style.css"> -->
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
-    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-  <!-- CSS here -->
-  <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
-  <link rel="stylesheet" href="assets/css/slicknav.css" />
-  <link rel="stylesheet" href="assets/css/flaticon.css" />
-  <link rel="stylesheet" href="assets/css/gijgo.css" />
-  <link rel="stylesheet" href="assets/css/animate.min.css" />
-  <link rel="stylesheet" href="assets/css/animated-headline.css" />
-  <link rel="stylesheet" href="assets/css/magnific-popup.css" />
-  <link rel="stylesheet" href="assets/css/fontawesome-all.min.css" />
-  <link rel="stylesheet" href="assets/css/themify-icons.css" />
-  <link rel="stylesheet" href="assets/css/slick.css" />
-  <link rel="stylesheet" href="assets/css/nice-select.css" />
-  <link rel="stylesheet" href="assets/css/style.css" /> -->
-  <!-- FontAwsome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+  <!-- Font Awesome CSS for icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+
+  <!-- Add your custom stylesheets here if needed -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Allura&family=Josefin+Sans&family=Lato:ital,wght@1,300&family=Roboto+Serif:opsz@8..144&family=Ysabeau+SC&display=swap" rel="stylesheet" />
+  <link href="register_style.css" rel="stylesheet" />
 </head>
 
 <body>
-  <!-- Navbar Start -->
-  <?php
-  include 'styles\components\navbar.php';
-  ?>
-  <!-- Navbar End -->
-
   <!-- Section: Design Block -->
   <section class="text-center">
     <!-- Background image -->
     <div class="p-5 bg-image" style="
           background-image: url('https://mdbootstrap.com/img/new/textures/full/171.jpg');
-          height: 300px;
+          height: 200px;
         "></div>
     <!-- Background image -->
 
-    <div class="card mx-4 mx-md-5 shadow-5-strong" style="
+    <div class="card mx-4 mx-md-5 shadow-5-strong mx-auto" style="
           margin-top: -100px;
           background: hsla(0, 0%, 100%, 0.8);
           backdrop-filter: blur(30px);
         ">
-      <div class="card-body py-5 px-md-5">
+      <div class="card-body">
         <div class="row d-flex justify-content-center">
-          <div class="col-lg-8">
+          <div class="col-lg-5">
             <h2 class="fw-bold mb-5 text-3xl sm:text-5xl lg:text-8xl font-black">
               Register
             </h2>
             <form>
               <!-- 2 column grid layout with text inputs for the first and last names -->
               <div class="row">
-                <div class="col-md-6 mb-4">
-                  <div class="form-outline">
-                    <input type="text" id="form3Example1" class="form-control" />
-                    <label class="form-label" for="form3Example1">First name</label>
+                <div class="col-md-20 mt-1">
+                  <div class="form-group">
+                    <input type="email" name="email" required id="form3Example1" class="form-control w-100" placeholder=" " />
+                    <label class="form-label" for="form3Example1">Full name</label>
                   </div>
                 </div>
-                <div class="col-md-6 mb-4">
-                  <div class="form-outline">
-                    <input type="text" id="form3Example2" class="form-control" />
-                    <label class="form-label" for="form3Example2">Last name</label>
+                <div class="col-md-20 mt-1">
+                  <div class="form-group">
+                    <input type="email" name="email" required id="form3Example1" class="form-control w-100" placeholder=" " />
+                    <label class="form-label" for="form3Example1">Email</label>
                   </div>
                 </div>
               </div>
 
               <!-- Email input -->
-              <div class="form-outline mb-4">
-                <input type="email" id="form3Example3" class="form-control" />
-                <label class="form-label" for="form3Example3">Email address</label>
+              <div class="column">
+                <div class="col-md-10 mt-1">
+                  <div class="form-group">
+                    <input type="email" name="email" required id="form3Example1" class="form-control w-100" placeholder=" " />
+                    <label class="form-label" for="form3Example1">Phone</label>
+                  </div>
+                </div>
+                <div class="col-md-10 mt-1">
+                  <div class="form-group">
+                    <input type="email" name="email" required id="form3Example1" class="form-control w-100" placeholder=" " />
+                    <label class="form-label" for="form3Example1">Phone</label>
+                  </div>
+                </div>
               </div>
 
               <!-- Password input -->
-              <div class="form-outline mb-4">
-                <input type="password" id="form3Example4" class="form-control" />
-                <label class="form-label" for="form3Example4">Password</label>
+              <div class="col-md-20 mt-1">
+                <div class="form-group">
+                  <input type="email" name="email" required id="form3Example1" class="form-control w-100" placeholder=" " />
+                  <label class="form-label" for="form3Example1">Gender</label>
+                </div>
               </div>
 
               <!-- Checkbox -->
