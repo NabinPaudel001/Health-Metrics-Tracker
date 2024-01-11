d57f0826bbef6a55fcc47d62b217e3ecffe50c67
SheikhAman <abamsheikh@gmail.com>
2024-01-11 17:08:46 +0545

    simple dashboard template design

--- login.php
+++ login.php
@@ -87,7 +87,6 @@ if (isset($_POST['login'])) {
 </head>
 
 <body>
-
   <section class="h-100 gradient-form " style="background-color: #eee;">
     <div class="container py-5 h-100 ">
       <div class="row d-flex justify-content-center align-items-center h-100 ">
